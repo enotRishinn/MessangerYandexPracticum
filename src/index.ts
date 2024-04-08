add460bdcd79148dfb509edca6d097680a342ba0
Victoriia Emtseva <enot.rishinn@yandex.ru>
2024-04-09 01:13:58 +0300

    develop: chat page

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,26 @@ import * as Components from './components';
 import * as Pages from './pages';
 
 const pages: { [key: string]: string[] } = {
-  '/login': [Pages.LoginPage],
-  '/register': [Pages.RegisterPage],
-  '/505': [ Pages.BugFixErrorPage ],
-  '/404': [ Pages.NotFoundErrorPage ],
+    '/login': [Pages.LoginPage],
+    '/register': [Pages.RegisterPage],
+    '/chat-page': [Pages.ChatPage],
+    '/505': [ Pages.BugFixErrorPage ],
+    '/404': [ Pages.NotFoundErrorPage ],
 };
 
 Object.entries(Components).forEach(([ name, component ]) => {
-  Handlebars.registerPartial(name, component);
+    Handlebars.registerPartial(name, component);
 });
 
 function navigate(path: string) {
-  const [ source, args ] = pages[path];
-  const handlebarsFunct = Handlebars.compile(source);
-  document.body.innerHTML = handlebarsFunct(args);
+    if (!pages[path]) {
+        window.location.pathname = '/login';
+        navigate('/login');
+    } else {
+        const [ source, args ] = pages[path];
+        const handlebarsFunct = Handlebars.compile(source);
+        document.body.innerHTML = handlebarsFunct(args);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => navigate(window.location.pathname));
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { NotFoundErrorPage } from './404-page/404-page';
 export { BugFixErrorPage } from './500-page/500-page';
 export { LoginPage } from './login-page/login-page';
 export { RegisterPage } from './register-page/register-page';
+export { ChatPage } from './chat-page/chat-page';
