e51e6e4497cec912043a8f302ed719fe3b91aacd
Victoriia Emtseva <enot.rishinn@yandex.ru>
2024-04-09 21:55:32 +0300

    fix: spaces and end of lines

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,4 +5,4 @@ export { DialogTitle } from './dialog-title/dialog-title';
 export { Button } from './button/button';
 export { InputField } from './input-field/input-field';
 export { Avatar } from './avatar/avatar';
-export { ProfileDataElement } from './profile-data-element/profile-data-element'
+export { ProfileDataElement } from './profile-data-element/profile-data-element';
