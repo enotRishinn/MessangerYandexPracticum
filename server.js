4c8454524eadb82679c410b0797cbd9e90b1e8e7
Victoriia Emtseva <enot.rishinn@yandex.ru>
2024-04-09 04:55:53 +0300

    develop: express

---
+++ server.js
@@ -0,0 +1,12 @@
+import path from 'path';
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(PORT, function () {
+    console.log('Приложение запущено на порту 3000!');
+});
